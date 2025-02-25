37d7f533047b9110abb28c89ebf07b5a264abc02
Josue Ibarra <jibarr21@ext.uber.com>
2025-02-24 16:21:06 -0800

    add json validation

--- scripts/fetch-releases.sh
+++ scripts/fetch-releases.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# This script downloads the latests release data from cadence-workflow repositories
 
+# This script downloads the latests release data from cadence-workflow repositories
  gh api \
   -H "Accept: application/vnd.github+json" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
@@ -17,3 +17,8 @@
   -H "Accept: application/vnd.github+json" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   /repos/cadence-workflow/cadence-java-client/releases > static/data/releases/cadence-java-client.json
+
+# Validate json files, if any of the commands fail, the script will exit with a non-zero status
+cat static/data/releases/cadence.json | jq '.[] | has("tag_name") and has("body")' -e > /dev/null
+cat static/data/releases/cadence-go-client.json | jq '.[] | has("tag_name") and has("body")' -e > /dev/null
+cat static/data/releases/cadence-java-client.json | jq '.[] | has("tag_name") and has("body")' -e > /dev/null
